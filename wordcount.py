ad183c724d3fddea4326d5a383db16a556f7adc6
Leah Ewing <leahewing@live.com>
2021-05-03 17:04:25 +0000

    Commit before lunch!

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,14 @@
 # put your code here.
+def wordcount(input):
+    #input = open("test.txt")
+    input_var = open(input)
+    letter_counts = {}
+
+    for line in input:
+        line = line.split(' ')
+        letter_counts[line] = letter_counts.get(line, 0) + 1
+
+    return letter_counts
+
+print(wordcount("test.txt"))
+
